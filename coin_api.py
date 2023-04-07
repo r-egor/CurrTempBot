d88fe742e52a001bba0d7bff351951206c1acd43
egor <rukhovets@gmail.com>
2023-04-07 17:05:41 +0300

    Improvement Check status code

--- coin_api.py
+++ coin_api.py
@@ -1,27 +1,26 @@
 import requests
 import settings_bot
 
+
 def get_crypto_prices():
-    # Get Crypto
+    # Get API Crypto
     get_crypto = requests.get(settings_bot.crypto)
+    # Check status code
+    if get_crypto.status_code != 200:
+        return "⛔️Crypto"
+
     data = get_crypto.json()
 
-    if get_crypto.status_code == 200:
-        # BTC
-        bitcoin_price = str(data["bitcoin"]["usd"]//1000) + "k"
-        # ETH
-        ethereum_price = str(round(data["ethereum"]["usd"]/1000, 2)) + "k"
-        # TON
-        the_open_network = data["the-open-network"]["usd"]
+    # BTC
+    bitcoin_price = str(data["bitcoin"]["usd"] // 1000) + "k"
+    # ETH
+    ethereum_price = str(round(data["ethereum"]["usd"] / 1000, 2)) + "k"
+    # TON
+    the_open_network = data["the-open-network"]["usd"]
 
-        # Message
-        message_text = f"📈BTC: {bitcoin_price}|" \
-                       f"ETH: {ethereum_price}|" \
-                       f"TON: {the_open_network}"
-    else:
-        message_text = "⛔️Crypto"
+    # Message
+    message_text = f"📈BTC: {bitcoin_price}|" \
+                   f"ETH: {ethereum_price}|" \
+                   f"TON: {the_open_network}"
 
     return message_text
-
-
-
