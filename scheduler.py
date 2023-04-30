17208e58cb98d1237331ae0646a651105e5e17df
egor <rukhovets@gmail.com>
2023-04-30 20:56:00 +0300

    Added function # If the user has blocked the bot, delete the user from the database

--- scheduler.py
+++ scheduler.py
@@ -13,17 +13,22 @@ def send_daily_message():
     database_users = DatabaseUsers()
     users = database_users.get_user()
     for user in users:
-        # User greeting
-        greeting = f'Добрый день, {user[1]}'
-        # Currency rate
-        currency = currency_api.get_currency_rate()
-        # Crypto price
-        crypto = coin_api.get_crypto_prices()
-        # Weather
-        weather = weather_api.get_weather_forecast()
-        # Send daily message
-        daily_message = f"{greeting}\n\n{currency}\n\n{weather}\n\n{crypto}"
-        bot.send_message(user[0], daily_message)
+        try:
+            # User greeting
+            greeting = f'Добрый день, {user[1]}'
+            # Currency rate
+            currency = currency_api.get_currency_rate()
+            # Crypto price
+            crypto = coin_api.get_crypto_prices()
+            # Weather
+            weather = weather_api.get_weather_forecast()
+            # Send daily message
+            daily_message = f"{greeting}\n\n{currency}\n\n{weather}\n\n{crypto}"
+            bot.send_message(user[0], daily_message)
+        except telebot.apihelper.ApiTelegramException as error:
+            # If the user has blocked the bot, delete the user from the database
+            if error.result.status_code == 403:
+                database_users.delete_user(user[0])
 
 # Schedule the daily message to be sent every day at 11:00 and 16:00
 schedule.every().day.at("11:00").do(send_daily_message)
